911552e4c66c8d459ab8f750cfaac38f64723ea7
‘zhaoyanda’ <1758259198@qq.com>
2023-09-11 09:35:17 +0800

    feat: 测试

--- src/pages/Dashboard/components/DemoCom.jsx
+++ src/pages/Dashboard/components/DemoCom.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 export default function DemoCom() {
   useEffect(() => {
-    alert('这是后端后台代码')
+    console.log('组件挂行了');
+    return () => {
+      console.log('组件卖了');
+    }
   }, [])
   return (
     <div></div>
